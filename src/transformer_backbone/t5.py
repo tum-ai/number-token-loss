2468bbf3d66ed86925052b7d0fe1f435ab946ec1
Anna Ketteler <anna.ketteler@ownly.de>
2024-07-18 15:10:09 +0200

    make T5 model inherit from T5ForConditionalGeneration

--- src/transformer_backbone/t5.py
+++ src/transformer_backbone/t5.py
@@ -1,18 +1,54 @@
+from typing import Optional, Tuple, Union
 from transformers import T5Tokenizer, T5Model, T5ForConditionalGeneration
+from transformers.modeling_outputs import Seq2SeqLMOutput
 import torch
 import torch.nn as nn
 
-class T5RegressionModel(nn.Module):
+class T5RegressionModel(T5ForConditionalGeneration):
     def __init__(self, pretrained_model_name='t5-small', num_output=1):
-        super(T5RegressionModel, self).__init__()
-        self.model = T5ForConditionalGeneration.from_pretrained(pretrained_model_name)
-        self.regression_head = nn.Linear(self.model.config.d_model, num_output)
+        super(T5RegressionModel, self).__init__(pretrained_model_name)
+        self.regression_head = nn.Linear(self.config.d_model, num_output)
+        
+    def forward(
+        self,
+        input_ids: Optional[torch.LongTensor] = None,
+        attention_mask: Optional[torch.FloatTensor] = None,
+        decoder_input_ids: Optional[torch.LongTensor] = None,
+        decoder_attention_mask: Optional[torch.BoolTensor] = None,
+        head_mask: Optional[torch.FloatTensor] = None,
+        decoder_head_mask: Optional[torch.FloatTensor] = None,
+        cross_attn_head_mask: Optional[torch.Tensor] = None,
+        encoder_outputs: Optional[Tuple[Tuple[torch.Tensor]]] = None,
+        past_key_values: Optional[Tuple[Tuple[torch.Tensor]]] = None,
+        inputs_embeds: Optional[torch.FloatTensor] = None,
+        decoder_inputs_embeds: Optional[torch.FloatTensor] = None,
+        labels: Optional[torch.LongTensor] = None,
+        use_cache: Optional[bool] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        return_dict: Optional[bool] = None,
+    ) -> Union[Tuple[torch.FloatTensor], Seq2SeqLMOutput]:
+        """Overwrites forward method of parent class T5ForConditionalGeneration. Computes embeddings
+        from input_ids and stores them to inputs_embeds. Argument inputs_embeds will not persist.
+        """
+        # create embeddings
+        inputs_embeds = self.shared(input_ids)
+        
+        # call super().forward()
+        outputs = super(T5RegressionModel, self).forward(
+            inputs_embeds=inputs_embeds,
+            attention_mask=attention_mask
+        )
 
-    def forward(self, input_ids, attention_mask=None):
+        hidden_states = outputs[0]
+        
+        return hidden_states
+    
+        """def forward(self, input_ids, attention_mask=None):
         encoder_outputs = self.model.encoder(input_ids=input_ids, attention_mask=attention_mask)
         hidden_states = encoder_outputs.last_hidden_state
 
         # hidden state of the last token is output
         sequence_output = hidden_states[:, -1, :]
 
-        return sequence_output
+        return sequence_output"""
