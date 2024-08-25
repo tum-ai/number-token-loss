e27b73040626de17c960ea988727c56a36388f16
kchlodny <kacper.chlodny@outlook.com>
2024-08-25 11:55:46 +0200

    Add NumberTokenLoss again after main rebase.
    Former-commit-id: aadba329726f727762260f09ae5b9a197b739a3b

---
+++ src/number_token_loss.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn.functional as F
+from torch._tensor import Tensor
+from src.encoding_decoding.numerical_encodings import encoding_to_number
+class NumberTokenLoss:
+    def __init__(self, tokenizer, loss_order=2):
+        self.tokenizer = tokenizer
+        self.order = loss_order
+        hashed_num_tokens = set(self.tokenizer.num_tokens)
+        self.nvocab = torch.tensor(
+            [encoding_to_number(token) if token in hashed_num_tokens else float('nan') for token in self.tokenizer.get_vocab()],
+            dtype=torch.float32
+        )
+
+    def forward(self, logits: Tensor, labels: Tensor):
+
+        # Create a mask to filter out non-digit tokens
+        number_tokens = ~torch.isnan(self.nvocab)
+        logits = logits[:, :, number_tokens]
+
+        # Compute the weighted average of number tokens (yhat)
+        softmaxed = F.softmax(logits, dim=-1)
+        yhat = torch.sum(softmaxed * self.nvocab[number_tokens], dim=-1)
+        y = self.nvocab[labels]
+
+        # Compute the final loss function
+        loss = torch.mean((torch.abs(y - yhat) ** self.order)) ** (1 / self.order)
+        return float(loss)
