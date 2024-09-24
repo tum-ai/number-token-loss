fixed scaling of number token loss

src/transformer_backbone/t5/t5_rt.py
@@ -83,8 +83,7 @@ class T5RegressionModelRT(T5ForConditionalGeneration):
             # number_token_loss = torch.log10(number_token_loss + 1)
             outputs["number_loss"] = number_token_loss
             outputs["token_loss"] = outputs.loss
-            outputs.loss = (1.0 - self.number_token_loss.weight) * outputs.loss + \
-                        self.number_token_loss.weight * number_token_loss
+            outputs.loss = outputs.loss + self.number_token_loss.weight * number_token_loss
         return outputs
 
 