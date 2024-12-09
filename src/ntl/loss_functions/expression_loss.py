eab44f0b803707669ef1e100e779a809a8268be5
Lars Pennig <lpennig@t-online.de>
2024-12-09 20:45:48 +0100

    improved implementation

--- src/ntl/loss_functions/expression_loss.py
+++ src/ntl/loss_functions/expression_loss.py
@@ -18,15 +18,16 @@ class ExpressionLoss:
         self.end_token_id = self.tokenizer.convert_tokens_to_ids(self.expression_tokens[1])
         self.equal_token_id = self.tokenizer.convert_tokens_to_ids('=')
         self.operator_ids = self.tokenizer.convert_tokens_to_ids(['+', '-', '*'])
+        self.underscore_id = self.tokenizer.convert_tokens_to_ids('_')
         
         # create a tensor of shape (vocab_size,) with the number tokens replaced by their corresponding number
         self.nvocab = torch.full((vocab_size,), float("nan"), device=device)
-        self.number_tokens = ~torch.isnan(self.nvocab)
-
 
         for token, id in self.tokenizer.get_vocab().items():
             if token in hashed_num_tokens:
                 self.nvocab[id] = self.tokenizer.decode_number_token(token, ignore_order=True)
+                
+        self.number_tokens = ~torch.isnan(self.nvocab)
                 
     def convert_logit_seq_to_number(self, logits: Tensor):
         # extract weighted number according to probability
@@ -39,9 +40,16 @@ class ExpressionLoss:
         y_hat = torch.sum(reversed_y_hat_i * powers_of_10)
         return y_hat
     
-    def extract_parts(self, logits: Tensor, labels: Tensor):
-        return 0 
-
+    def apply_operator(self, first_num, second_num, operator):
+        if operator == '+':
+            return first_num + second_num
+        elif operator == '-':
+            return first_num - second_num
+        elif operator == '*':
+            return first_num * second_num
+        else:
+            raise ValueError(f"Operator {operator} is not supported")
+          
     def forward(self, logits: Tensor, labels: Tensor):
         if logits.numel() == 0:
             raise ValueError("Logits passed to the NumberTokenLoss are empty!")
@@ -65,19 +73,22 @@ class ExpressionLoss:
         _, op_indices = torch.where(operator_locs)
         
         loss = 0
-        for b, s, e, eq, op in zip(batch_indices, start_indices, end_indices, equal_indices, op_indices):
+        for batch, start, end, eq, op in zip(batch_indices, start_indices, end_indices, equal_indices, op_indices):
             # extract operator
-            operator_id = labels[b, op]
+            operator_id = labels[batch, op]
             operator = self.tokenizer.convert_ids_to_tokens(operator_id.item())
             
             # extract solutions
-            solution = self.tokenizer.convert_ids_to_tokens(labels[b, eq+1:e])
+            solution = self.tokenizer.convert_ids_to_tokens(labels[batch, eq+1:end])
                         
             # extract predicted first number 
-            first_num = self.convert_logit_seq_to_number(logits[b, s+1:op,:])
-            second_num = self.convert_logit_seq_to_number(logits[b, op+1:eq,:])
-            pred_solution = self.convert_logit_seq_to_number(logits[b, eq+1:e,:])
+            first_num = self.convert_logit_seq_to_number(logits[batch, start+2:op,:]) # @TODO: for some reason the first token is always a '_' token.
+            second_num = self.convert_logit_seq_to_number(logits[batch, op+1:eq,:])
+            pred_solution = self.convert_logit_seq_to_number(logits[batch, eq+1:end,:])
             
-            loss += self.loss_function(first_num + second_num, pred_solution)
+            # compute result based on predicted numbers
+            consistent_solution = self.apply_operator(first_num, second_num, operator)
+            
+            loss += self.loss_function(consistent_solution, pred_solution)
             
         return loss
