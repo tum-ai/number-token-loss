d03faad7771eb88e842f934ebe438e85057b85da
Lars Pennig <lpennig@t-online.de>
2024-12-09 17:05:03 +0100

    added first version of expression ntl

--- src/ntl/transformer_backbone/t5/t5_vanilla_for_number_token_loss.py
+++ src/ntl/transformer_backbone/t5/t5_vanilla_for_number_token_loss.py
@@ -3,14 +3,19 @@ from transformers.modeling_outputs import Seq2SeqLMOutput
 import torch
 from typing import Optional, Tuple, Union
 from ntl.loss_functions.number_token_loss import NumberTokenLoss
+from ntl.loss_functions.expression_loss import ExpressionLoss
 
 
 class T5VanillaForNumberTokenLoss(T5ForConditionalGeneration):
-    def __init__(self, config, number_token_loss: NumberTokenLoss = None):
+    def __init__(self, config, number_token_loss: NumberTokenLoss = None, expression_loss = ExpressionLoss):
         super().__init__(config)
+        super().resize_token_embeddings(config.vocab_size, pad_to_multiple_of=64 if torch.cuda.is_available() else 1)
 
-        # Initialize NumberTokenLoss
+        # Initialize additional losses
         self.number_token_loss = number_token_loss
+        self.expression_loss = expression_loss
+    
+        
 
     def forward(
             self,
@@ -57,4 +62,9 @@ class T5VanillaForNumberTokenLoss(T5ForConditionalGeneration):
             outputs["number_loss"] = number_token_loss
             outputs["token_loss"] = outputs.loss
             outputs.loss = outputs.loss + self.number_token_loss.weight * number_token_loss
+            
+        if labels is not None and self.expression_loss is not None:
+            expression_loss = self.expression_loss.forward(outputs.logits, labels)
+            outputs["expression_loss"] = expression_loss
+            outputs.loss = outputs.loss + self.expression_loss.weight * expression_loss
         return outputs
