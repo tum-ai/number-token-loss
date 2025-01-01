ce243f4b72c563af9e360046d94555c9aeef904f
ad045 <adrian.dendorfer@gmail.com>
2025-01-01 20:18:30 +0100

    added number_token_selector.py

---
+++ src/ntl/utils/number_token_selector.py
@@ -0,0 +1,31 @@
+import torch
+import torch.nn.functional as F
+from torch._tensor import Tensor
+
+from ntl.tokenizer.abstract_tokenizer import NumberEncodingTokenizer
+
+class NumberTokenSelector:
+    '''
+    Select number tokens 
+    '''
+    
+    def __init__(self, tokenizer: NumberEncodingTokenizer, nvocab):
+        self.tokenizer = tokenizer
+        self.nvocab = nvocab
+        hashed_num_tokens = set(self.tokenizer.get_num_tokens())
+        
+        for token, id in self.tokenizer.get_vocab().items():
+            if token in hashed_num_tokens:
+                self.nvocab[id] = self.tokenizer.decode_number_token(token, ignore_order=True)
+
+
+    def select_number_tokens(self, logits: Tensor, labels: Tensor):
+        
+        # Create a mask to filter out non-digit tokens and labels
+        number_tokens = ~torch.isnan(self.nvocab)
+        logits = logits[:, :, number_tokens] 
+        labels = labels.masked_fill(labels == -100, 0)
+
+        return logits, labels, number_tokens
+
+
